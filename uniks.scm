a953a2dade7ba252d350d061157ecac0d7267619
li <li@maisiliym.uniks>
2021-06-15 18:10:22 +0700

    (snap preDatomMess)

--- uniks.scm
+++ uniks.scm
@@ -1,13 +1,33 @@
-(define-module (uniks)
-  #:use-module (oop goops)
-  #:export (PriKriom
-	    Krimyn))
+(define-module (uniks))
+(use-modules (oop goops))
+(export (<prikriom>
+	  <krimyn>))
 
-(define-class PriKriom ()
+
+(define-class <raizyn> ()
+  (metaneksis #:init-keyword #:metaneksis)
+  (neksis #:init-keyword #:neksis)
+  (krimyn #:init-keyword #:krimyn))
+
+(define-class <prikriom> ()
+  (ful #:init-keyword #:ful)
+  (ssh #:init-keyword #:ssh)
+  (keygrip #:init-keyword #:keygrip))
+
+(define-class <krimyn> ()
+  (neim #:init-keyword #:neim)
   (spici #:init-keyword #:spici)
-  (valiu #:init-keyword #:valiu))
+  (trost #:init-keyword #:trost)
+  (saiz #:init-keyword #:saiz)
+  (prikriom ; #:type <prikriom>
+   #:init-Keyword #:prikriom))
 
-(define-class Krimyn ()
-  (pri-kriomz ; #:type (list-of PriKriom)
-   #:init-Keyword #:pri-kriomz))
+(define-class <neksis> ()
+  (neim #:init-keyword #:neim)
+  (spici #:init-keyword #:spici)
+  (saiz #:init-keyword #:saiz)
+  (prikriom ; #:type <prikriom>
+   #:init-Keyword #:prikriom)
+  (krimynz #:init-keyword #:krimynz)
+  (keygrip #:init-keyword #:keygrip))
 
