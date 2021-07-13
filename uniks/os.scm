5ac6f434f1cd1c740f5117577e87b6dae467e116
li <li@maisiliym.uniks>
2021-07-13 15:51:32 +0700

    (snap wip)

--- uniks/os.scm
+++ uniks/os.scm
@@ -7,16 +7,14 @@
 	     (gnu services)
 	     (gnu services base)
 	     (gnu services pm)
-	     ((gnu services desktop)
-	      #:select (%desktop-services) #:prefix guix:)
+	     ((gnu services desktop) #:select (%desktop-services))
 	     (gnu services shepherd)
 	     (gnu services sddm)
 	     (gnu services xorg)
 	     (gnu services networking)
 	     (gnu services sound)
 	     (gnu services avahi)
-	     ((gnu system file-systems)
-              #:select (%elogind-file-systems file-system))
+	     ((gnu system file-systems) #:select (%elogind-file-systems file-system))
 	     (gnu system)
 	     (gnu system shadow)
 	     (gnu system pam)
@@ -40,24 +38,10 @@
    services))
 
 (define %unwanted-guix-desktop-services
-  (list avahi-service-type
+service-type
 	gdm-service-type))
 
-(define %modified-guix-desktop-services
-  (modify-services
-      (remove-services guix:%desktop-services %unwanted-guix-desktop-services)
-    (guix-service-type
-     config =>
-     (guix-configuration
-      (inherit config)
-      (substitute-urls
-       (append
-	%default-substitute-urls ; To be removed
-	(->substitute-urls metaneksys)))
-      (authorized-keys
-       (append
-	%guix-authorized-keys %default-authorized-guix-keys))
-      (extra-options '("--max-jobs=2"))))))
+
 
 (define %edj-services
   (append
@@ -100,21 +84,63 @@
 	   (file-append swaylock "/bin/swaylock"))
      (remove-members %setuid-programs %unwanted-setuid-programs))))
 
+(define-method (->stock-services (spici <spici>)
+				(saiz <integer>)
+				(ssh-authorized-keys <list>))
+  (let*
+      ()
+    (remove-services %desktop-services %unwanted-guix-desktop-services)))
+
+(define-method (->ssh-service (authorized-keys <list>))
+  (ssh-service
+   (service
+    openssh-service-type
+    (openssh-configuration
+     (openssh openssh-sans-x)
+     (password-authentication? #false)
+     (permit-root-login 'prohibit-password)
+     (authorized-keys ssh-authorized-keys)))))
+
+(define-method (->services (spici <string>) (saiz <integer>)
+			   (metaneksys-substitute-urls <list>)
+			   (metaneksys-guix-keys <list>)
+			   (ssh-authorized-keys <list>))
+  (let*
+      ((stock-services (->stock-services spici saiz))
+       (substitute-urls (append %default-substitute-urls metaneksys-substitute-urls))
+       (authorized-keys (append %guix-authorized-keys ssh-authorized-keys))
+       (ssh-service (->ssh-service ssh-authorized-keys
+				   ))
+       (base-services (->stock-services spici saiz))
+       (unmodified-services (append base-services stock-services)))
+    (modify-services unmodified-services
+      (guix-service-type
+       config =>
+       (guix-configuration
+	(inherit config)
+	(substitute-urls substitute-urls)
+	(authorized-keys authorized-keys))))))
+
+(define-method (->kernel-arguments (neksys <neksys>))
+  (cons "intel_pstate=disable" %default-kernel-arguments))
+
 (define-method (->os (rairyn <raizyn>))
   (let*
       ((neksys (->neksys raizyn))
+       (neksys-spici (->spici <string>))
+       (neksys-saiz (->saiz <string>))
+       (krimynz (->krimynz raizyn))
        (host-name (->neim neksys))
-       (kernel-arguments
-	(cons "intel_pstate=disable" %default-kernel-arguments))
+       (kernel-arguments (->kernel-arguments neksys))
        (authorized-keys (->authorized-keys neksys krimynz))
-       (ssh-service
-	(service
-	 openssh-service-type
-	 (openssh-configuration
-	  (openssh openssh-sans-x)
-	  (password-authentication? #false)
-	  (permit-root-login 'without-password)
-	  (authorized-keys authorized-keys))))
+       (metaneksys-substitute-urls (->substitute-urls metaneksys))
+       (metaneksys-guix-keys (->guix-keys metaneksys))
+       (ssh-authorized-keys (->authorized-keys neksys krimynz))
+       (services (->services neksys-spici neksys-saiz
+			     metaneksys-substitute-urls
+			     metaneksys-guix-keys
+			     ssh-authorized-keys ))
+       
        (services (cons* ssh-service %edj-services))
        (packages (append %uniks-base-packages))
        (users
